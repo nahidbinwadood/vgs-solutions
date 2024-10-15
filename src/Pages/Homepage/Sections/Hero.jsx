68bcd9546e59e8389e93b4f466ad1159b37f55d5
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-15 14:44:04 +0600

    updated title space

--- src/Pages/Homepage/Sections/Hero.jsx
+++ src/Pages/Homepage/Sections/Hero.jsx
@@ -42,16 +42,16 @@ const Hero = () => {
             data-aos="zoom-in"
             data-aos-duration="1200"
             id="welcome-heading"
-            className="text-2xl sm:text-3xl mb-1 md:text-4xl lg:text-5xl 2xl:text-6xl leading-snug md:leading-tight"
+            className="text-2xl sm:text-3xl mb-2 md:text-4xl lg:text-5xl 2xl:text-6xl leading-snug md:leading-tight"
           >
             Welcome to VGS,
           </h1>
           <h2
             data-aos="zoom-in"
             data-aos-duration="1300"
-            className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl font-bold leading-snug md:leading-tight lg:leading-[1.2] xl:leading-[1.25]"
+            className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl font-bold leading-normal sm:leading-normal md:leading-snug lg:leading-tight 2xl:leading-tight"
           >
-            Your Trusted Partner in Global Supply Chain Solutions
+            Your Trusted Partner in Global <br /> Supply Chain Solutions
           </h2>
         </header>
 
